626af100d4e0679ed906f38b15f898c90499c7f1
Edward Estandarte <estandarte.edward@gmail.com>
2021-04-23 18:59:48 -0400

    Updates

---
+++ precache-manifest.6b714e2f800be850d7e8ebeafefb874d.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9cf060f913ad53cd87cf191304351bbc",
+    "url": "/test-react-portfolio/index.html"
+  },
+  {
+    "revision": "4e259174c419dd86340c",
+    "url": "/test-react-portfolio/static/css/main.cfb834ed.chunk.css"
+  },
+  {
+    "revision": "ec7dea27f2d267470dff",
+    "url": "/test-react-portfolio/static/js/2.102548e6.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/test-react-portfolio/static/js/2.102548e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e259174c419dd86340c",
+    "url": "/test-react-portfolio/static/js/main.6a9fcd7c.chunk.js"
+  },
+  {
+    "revision": "fa9bf8621e255fb7b6a3",
+    "url": "/test-react-portfolio/static/js/runtime-main.1832ce02.js"
+  },
+  {
+    "revision": "a19f2b67a0540f784bf091db8e36c82a",
+    "url": "/test-react-portfolio/static/media/calculator.a19f2b67.jpg"
+  },
+  {
+    "revision": "402a471b5aad190a33b649f1368f5de1",
+    "url": "/test-react-portfolio/static/media/cover-image.402a471b.jpg"
+  },
+  {
+    "revision": "653c2b3d29f66a3c6e957518f3bd16f0",
+    "url": "/test-react-portfolio/static/media/led-wall.653c2b3d.jpg"
+  },
+  {
+    "revision": "ed494730868bd902e0ee98f1df1877e2",
+    "url": "/test-react-portfolio/static/media/pastel-puzzels.ed494730.jpg"
+  },
+  {
+    "revision": "938f8fe0fe2e5880b3ef528c5188a096",
+    "url": "/test-react-portfolio/static/media/run-buddy.938f8fe0.jpg"
+  },
+  {
+    "revision": "12557c9b108cf5e5347f8d838014b31e",
+    "url": "/test-react-portfolio/static/media/surf-report.12557c9b.jpg"
+  }
+]);
